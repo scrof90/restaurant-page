301cf84b564e08fca7d60dda245b1b544fa4d274
scrof90 <asknyshev@gmail.com>
2021-10-23 01:57:05 +0300

    configure rollup for 2 terser and babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,66 @@
 // rollup.config.js
+import { terser } from 'rollup-plugin-terser';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+
+const devMode = process.env.NODE_ENV === 'development';
+console.log(`${devMode ? 'development' : 'production'} mode bundle`);
 
 export default [
   {
-    input: './src/main.js',
+    // ES6
+    input: 'src/main.js',
+
+    watch: {
+      include: 'src/**',
+      clearScreen: false
+    },
 
     output: {
-      file: './build/bundle.js',
-
+      file: 'build/bundle.js',
       format: 'es',
-    },
+      sourcemap: devMode ? 'inline' : false,
+      plugins: [
+        terser({
+          ecma: 2020,
+          mangle: { toplevel: true },
+          compress: {
+            module: true,
+            toplevel: true,
+            unsafe_arrows: true,
+            drop_console: !devMode,
+            drop_debugger: !devMode
+          },
+          output: { quote_style: 1 }
+        })
+      ]
+    }
   },
+  {
+    // ES5
+    input: 'src/main.js',
+
+    plugins: [
+      // https://github.com/terser/terser#minify-options
+      terser({
+        ecma: '5',
+        mangle: true,
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true,
+          drop_console: !devMode,
+          drop_debugger: !devMode
+        }
+      }),
+      getBabelOutputPlugin({
+        presets: [['@babel/preset-env']]
+      })
+    ],
+
+    output: {
+      file: 'build/bundle-es5.js',
+      format: 'cjs',
+      sourcemap: false
+    }
+  }
 ];
