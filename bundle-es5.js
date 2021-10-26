3184a73bc092aa50a2de5157254a2430c0c3538e
scrof90 <asknyshev@gmail.com>
2021-10-27 01:45:49 +0300

    Updates

--- bundle-es5.js
+++ bundle-es5.js
@@ -1,4 +1,12 @@
-function t(t) {
+var t = [{
+  href: "https://www.artstation.com/artwork/aa6R8",
+  text: "Background art by Renaud ROCHE"
+}, {
+  href: "https://www.artstation.com/artwork/nlKG6",
+  text: "Additional art by J.Otto Szatmari"
+}];
+
+function e(t) {
   for (var _len = arguments.length, e = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
     e[_key - 1] = arguments[_key];
   }
@@ -8,17 +16,22 @@ function t(t) {
   });
 }
 
-function e(t, e, n) {
+function n(t, e, n) {
   var c = document.createElement("div");
   return c.classList.add("tab", e), t && c.classList.add("active"), c.textContent = n, c;
 }
 
-function n(t, e) {
-  var n = document.createElement("a");
-  return n.classList.add("attribution"), n.href = t, n.textContent = e, n;
+function c(t) {
+  var e = document.createElement("li"),
+      n = function (t, e) {
+    var n = document.createElement("a");
+    return n.classList.add("attribution"), n.href = t, n.textContent = e, n;
+  }(t.href, t.text);
+
+  return e.appendChild(n), e;
 }
 
-function c() {
+function o() {
   var t = document.querySelector(".js-content");
   t.innerHTML = "";
   !function (t) {
@@ -32,17 +45,17 @@ function c() {
   }(t, function () {
     var t = document.createElement("img");
     return t.classList.add("img-home"), t.src = "img/j-otto-szatmari-daygonebar-spacestation-jottoszatmari2016.jpg", t.alt = "Bar - Space Station by J.Otto Szatmari", t;
-  }(), o("h2", "Welcome to Saul Dyson's Bar!"), o("p", "With a spectacular dancing floor harboring the best DJs from all over the solar the system and a million energy credit view of a recently build <strong>Dyson sphere</strong>, enjoy a wide variety of unique meals that would satisfy even the most sophisticated digestive systems in the galaxy."), o("p", "We are always open"), o("p", "All species are welcome<y>*</y>"), o("p", "<y>*</y>the Ur-Quan will be forcefully escorted from the premises", "bottom-text"));
+  }(), a("h2", "Welcome to Saul Dyson's Bar!"), a("p", "With a spectacular dancing floor harboring the best DJs from all over the solar the system and a million energy credit view of a recently build <strong>Dyson sphere</strong>, enjoy a wide variety of unique meals that would satisfy even the most sophisticated digestive systems in the galaxy."), a("p", "We are always open"), a("p", "All species are welcome<y>*</y>"), a("p", "<y>*</y>the Ur-Quan will be forcefully escorted from the premises", "bottom-text"));
 }
 
-function o(t, e, n) {
+function a(t, e, n) {
   var c = document.createElement(t);
   return c.innerHTML = e, n && c.classList.add(n), c;
 }
 
 !function () {
-  var c = document.querySelector("body"),
-      o = function () {
+  var o = document.querySelector("body"),
+      a = function () {
     var t = document.createElement("header"),
         e = function () {
       var t = document.createElement("h1");
@@ -51,14 +64,14 @@ function o(t, e, n) {
 
     return t.appendChild(e), t;
   }(),
-      a = function () {
-    var n = document.createElement("nav"),
-        c = e(!0, "js-home", "Home"),
-        o = e(!1, "js-menu", "Menu"),
-        a = e(!1, "js-contacts", "Contacts");
-    return t(n, c, o, a), n;
-  }(),
       s = function () {
+    var t = document.createElement("nav"),
+        c = n(!0, "js-home", "Home"),
+        o = n(!1, "js-menu", "Menu"),
+        a = n(!1, "js-contacts", "Contacts");
+    return e(t, c, o, a), t;
+  }(),
+      i = function () {
     var t = document.createElement("main"),
         e = function () {
       var t = document.createElement("div");
@@ -67,18 +80,15 @@ function o(t, e, n) {
 
     return t.appendChild(e), t;
   }(),
-      i = function () {
-    var e = document.createElement("footer"),
-        c = function () {
-      var e = document.createElement("ul"),
-          c = document.createElement("li"),
-          o = n("https://www.artstation.com/artwork/aa6R8", "Background art by Renaud ROCHE");
-      c.appendChild(o);
-      var a = document.createElement("li"),
-          s = n("https://www.artstation.com/artwork/nlKG6", "Additional art by J.Otto Szatmari");
-      return a.appendChild(s), t(e, c, a), e;
-    }(),
+      r = function () {
+    var n = document.createElement("footer"),
         o = function () {
+      var e = document.createElement("ul");
+      return t.map(c).forEach(function (t) {
+        return e.appendChild(t);
+      }), e;
+    }(),
+        a = function () {
       var t = document.createElement("a");
       t.href = "https://github.com/scrof90";
 
@@ -90,16 +100,16 @@ function o(t, e, n) {
       return t.appendChild(e), t;
     }();
 
-    return t(c, o), e;
+    return e(n, o, a), n;
   }();
 
-  t(c, o, a, s, i);
-}(), c(), function () {
+  e(o, a, s, i, r);
+}(), o(), function () {
   var t = document.querySelector(".js-home"),
       e = document.querySelector(".js-menu"),
       n = document.querySelector(".js-contacts");
   t.addEventListener("click", function (t) {
-    t.target.classList.add("active"), e.classList.remove("active"), n.classList.remove("active"), c();
+    t.target.classList.add("active"), e.classList.remove("active"), n.classList.remove("active"), o();
   }), e.addEventListener("click", function (e) {
     e.target.classList.add("active"), t.classList.remove("active"), n.classList.remove("active"), function () {
       var t = document.querySelector(".js-content");
